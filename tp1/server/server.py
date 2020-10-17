0ef32131e7b720a877ab819b6533cebcab2e5e45
manudelca <manueldelcarril@gmail.com>
2020-10-16 23:37:58 -0300

    Server tipo netcat

--- tp1/server/server.py
+++ tp1/server/server.py
@@ -1,1 +1,22 @@
-print("Hola mundo!")
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.bind((socket.gethostname(), 8080))
+
+sock.listen(5)
+
+while True:
+
+    conn, addr = sock.accept()
+    if not conn:
+        break
+    while True:
+        data = conn.recv(1024)
+        if len(data) <= 0:
+            break
+        conn.send(data)
+
+
+sock.close()
+
