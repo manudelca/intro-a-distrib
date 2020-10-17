3425d98d283cde61153cc5cef84a8873722ea09a
manudelca <manueldelcarril@gmail.com>
2020-10-17 00:31:48 -0300

    Nuevo puerto

--- tp1/server/server.py
+++ tp1/server/server.py
@@ -2,12 +2,13 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock.bind(('0.0.0.0', 43908))
+sock.bind(('0.0.0.0', 8080))
 sock.listen(5)
 
 while True:
-
+    print("Waiting connection...")
     conn, addr = sock.accept()
+    print("Conection accepted!")
     if not conn:
         break
     while True:
